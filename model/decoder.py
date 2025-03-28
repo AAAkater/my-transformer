bf2a1c88ebe0b7a661c535987a54c538542abdf4
AAAkater <1318382761@qq.com>
2025-03-28 20:35:59 +0800

    ✨ feat: integrate TransformerEmbedding into Decoder and update forward method

--- model/decoder.py
+++ model/decoder.py
@@ -1,17 +1,25 @@
 from torch import Tensor, nn
 
 from model.block.my_decoder_layer import DecoderLayer
+from model.embedding.my_embedding import TransformerEmbedding
 
 
 class Decoder(nn.Module):
     def __init__(
         self,
+        dec_voc_size: int,
+        max_len: int,
         d_model: int,
         d_ff: int,
         n_head: int,
         n_layers: int,
     ):
         super(Decoder, self).__init__()
+        self.emb = TransformerEmbedding(
+            dec_voc_size,
+            d_model,
+            max_len,
+        )
         self.layers = nn.ModuleList(
             [
                 DecoderLayer(
@@ -22,17 +30,17 @@ class Decoder(nn.Module):
                 for _ in range(n_layers)
             ]
         )
+        self.linear = nn.Linear(d_model, dec_voc_size)
 
     def forward(
         self,
         dec: Tensor,
         enc_out: Tensor,
-        self_attn_mask,
-        dec_enc_attn_mask,
+        trg_mask: Tensor,
+        src_mask: Tensor,
     ):
+        trg: Tensor = self.emb(dec)
         for layer in self.layers:
-            dec, self_attn, dec_enc_attn = layer(
-                dec, enc_out, self_attn_mask, dec_enc_attn_mask
-            )
-
-        return dec, self_attn, dec_enc_attn
+            dec = layer(trg, enc_out, trg_mask, src_mask)
+        out_put: Tensor = self.linear(trg)
+        return out_put
